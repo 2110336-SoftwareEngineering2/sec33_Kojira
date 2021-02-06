87ea309d8795d0d11e119302c4e40ab2b14cd813
Megacodexz <megacodexz@gmail.com>
2021-02-06 16:22:10 +0700

    Change to export controller

--- nontserver/Controllers/NontOwnerController.js
+++ nontserver/Controllers/NontOwnerController.js
@@ -2,18 +2,20 @@
 
 const NontOwner = require("../Models/NontOwner");
 
-// POST /nontOwners
-const registerNontOwner = async (req, res) => {
-  // NEED validation
-  // NEED password hashing
-  try {
-    const nontOwnerAccount = await NontOwner.create(req.body);
-    return res.send(nontOwnerAccount._id);
-  } catch (error) {
-    return res.status(500).send("Something went wrong. Cannot create Nont owner.");
-  }
-};
+const controller = {
 
-module.exports = {
-  registerNontOwner,
-};
+  // POST /nontOwners
+  registerNontOwner: async (req, res) => {
+    // NEED validation
+    // NEED password hashing
+    try {
+      const nontOwnerAccount = await NontOwner.create(req.body);
+      return res.send(nontOwnerAccount._id);
+    } catch (error) {
+      return res.status(500).send("Something went wrong. Cannot create Nont owner.");
+    }
+  },
+
+}
+
+module.exports = controller;
--- nontserver/Controllers/NontSitterController.js
+++ nontserver/Controllers/NontSitterController.js
@@ -2,19 +2,22 @@
 
 const NontSitter = require('../Models/NontSitter');
 
-// POST /nontSitters
-const registerNontSitter = async (req, res) => {
-  // NEED validation
-  // NEED password hashing
-  try {
-    const nontSitterAccount = await NontSitter.create(req.body);
-    return res.send(nontSitterAccount._id);
-  } catch (error) {
-    return res.status(500).send("Something went wrong. Cannot create Nont owner.");
-  }
-};
+const controller = {
 
-module.exports = {
-  registerNontSitter,
-};
+  // POST /nontSitters
+  registerNontSitter: async (req, res) => {
+    // NEED validation
+    // NEED password hashing
+    try {
+      const nontSitterAccount = await NontSitter.create(req.body);
+      return res.send(nontSitterAccount._id);
+    } catch (error) {
+      return res.status(500).send("Something went wrong. Cannot create Nont owner.");
+    }
+  },
 
+}
+
+
+
+module.exports = controller;
