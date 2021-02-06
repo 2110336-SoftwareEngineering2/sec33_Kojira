43ef4485b8e61926d6eabc18db2d0457f81632c0
Megacodexz <megacodexz@gmail.com>
2021-02-06 17:09:03 +0700

    Use routes of nontOwners and nontSitters

--- nontserver/routes/index.js
+++ nontserver/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const nontOwners = require('./nontOwners');
+const nontSitters = require('./nontSitters');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/nontOwners', nontOwners);
+router.use('/nontSitters', nontSitters);
 
 module.exports = router;
