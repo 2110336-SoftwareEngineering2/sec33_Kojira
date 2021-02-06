9194ff01c226d4acb12bc27f538edb9335caad27
kombangkoe <kbdirifilation@gmail.com>
2021-02-06 13:23:44 +0700

    ready to merge

--- nontserver/routes/index.js
+++ nontserver/routes/index.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var router = express.Router();
+const loginRouter = require("./login");
 var db = require("../Controllers/dbconnector");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+router.use("/login", loginRouter);
 
 module.exports = router;
