85eba3849a5bf6fa2c34fd02fd71befa0e219397
Megacodexz <megacodexz@gmail.com>
2021-02-07 11:44:16 +0700

    Fix indentation in router file

--- nontclient/src/Router/Router.js
+++ nontclient/src/Router/Router.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Homepage from "../Components/Homepage/Homepage";
 
 function Router() {
-  return ( 
-		<BrowserRouter>
-			<Switch>
-				<Route path="/home" component={Homepage} />
-				<Redirect to="/home"/>
-			</Switch>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/home" component={Homepage} />
+        <Redirect to="/home" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
