c08df11eaaad2f825763b9e4983241239c61fbc3
Megacodexz <megacodexz@gmail.com>
2021-02-07 11:04:24 +0700

    Move router folder to /src/

---
+++ nontclient/src/Components/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() { 
+    return (
+      <h1 className="col">Nont Community of Pet Lovers</h1>
+    );
+  }
+}
+ 
+export default Homepage;
