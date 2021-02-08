bf3a700070a74099314f04a486059e74f4185b26
Megacodexz <megacodexz@gmail.com>
2021-02-08 16:41:35 +0700

    Create registration page

---
+++ nontclient/src/Components/Registration/Registration.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Registration.css';
+
+class Registration extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h1>Register Account</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+ 
+export default Registration;
