dda8bba6a6e8972fad218722cacccd79c32c1532
Megacodexz <megacodexz@gmail.com>
2021-02-09 21:32:05 +0700

    Fix margin of password input field

--- nontclient/src/Components/Registration/Registration.js
+++ nontclient/src/Components/Registration/Registration.js
@@ -14,14 +14,14 @@ class Registration extends Component {
           <input type="email" class="form-control" id="email-input" placeholder="name@example.com" />
         </div>
         <div className="row">
-          <div className="col-lg mx-4">
+          <div className="col-lg m-4">
             <label for="password-input" class="form-label">Password</label>
             <input type="password" id="password-input" class="form-control" aria-describedby="password-desc" />
             <div id="password-desc" class="form-text">
               Your password must be 8-32 characters long.
             </div>
           </div>
-          <div className="col-lg mx-4">
+          <div className="col-lg m-4">
             <label for="retype-password-input" class="form-label">Retype Password</label>
             <input type="password" id="retype-password-input" class="form-control" aria-describedby="retype-password-desc" />
             <div id="retype-password" class="form-text">
