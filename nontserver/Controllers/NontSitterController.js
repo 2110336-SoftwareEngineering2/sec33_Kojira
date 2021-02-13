f768ced3a05a6a2f1bbb713b2508a4532f9281ae
Megacodexz <megacodexz@gmail.com>
2021-02-13 20:19:44 +0700

    Change min and max chars of name in account

--- nontserver/Controllers/NontSitterController.js
+++ nontserver/Controllers/NontSitterController.js
@@ -11,7 +11,7 @@ const PASSWORD_HASHING_ROUNDS = 10;
 const validator = Joi.object({
   email: Joi.string().required().email(),
   password: Joi.string().required().min(8).max(32),
-  name: Joi.string().required().min(2).max(32),
+  name: Joi.string().required().min(1).max(64),
   phoneNumber: Joi.string().length(10).pattern(/^[0-9]+$/),
   bankAccount: Joi.string().length(10).pattern(/^[0-9]+$/),
 });
