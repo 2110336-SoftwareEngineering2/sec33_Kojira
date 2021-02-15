62025b7ee9dc22202742ac2c49540a716cdc6a70
kombangkoe <kbdirifilation@gmail.com>
2021-02-15 20:27:19 +0700

    thinking about not using reactstrap and use icon instead in the collapse navigation bar, simplified the state in NavigationBar class

--- nontclient/src/Components/NavigationBar/NavigationBar.js
+++ nontclient/src/Components/NavigationBar/NavigationBar.js
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavbarText,
-} from "reactstrap";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav } from "reactstrap";
 import NavigationLinks from "./NavigationLinks";
 import Contexts from "../../Utils/Context/Contexts";
 import styles from "./NavigationBar.module.css";
@@ -15,7 +8,6 @@ const UserTypeContext = Contexts.userTypeContext;
 
 const NavigationBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [userType, setUserType] = useState(null);
 
   const toggle = () => setIsOpen(!isOpen);
 
@@ -23,18 +15,19 @@ const NavigationBar = (props) => {
     <div>
       <UserTypeContext.Consumer>
         {(value) => {
-          setUserType(value.userType);
+          return (
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">Logo</NavbarBrand>
+              <NavbarToggler onClick={toggle} />
+              <Collapse isOpen={isOpen} navbar>
+                <Nav className="container-fluid" navbar>
+                  <NavigationLinks userType={value.userType} />
+                </Nav>
+              </Collapse>
+            </Navbar>
+          );
         }}
       </UserTypeContext.Consumer>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Logo</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="container-fluid" navbar>
-            <NavigationLinks userType={userType} />
-          </Nav>
-        </Collapse>
-      </Navbar>
     </div>
   );
 };
