16edd32d81d79ae501985696316b6307b8423050
jelly <32786620+puttimeth@users.noreply.github.com>
2021-02-20 09:27:36 +0700

    Shelter Manage Page beauty

--- nontclient/src/Components/Shelter/ShelterRow.js
+++ nontclient/src/Components/Shelter/ShelterRow.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./ShelterRow.css";
 
 const ShelterRow = (props) => {
     return (
-        <div className="row">
+        <div className="row mt-2 mb-2">
             {/* shelter view button */}
-            <div className="col m-4">
+            <div className="col-md-6 p-0">
                 <button 
                 type="button"
-                className="btn btn-primary" 
+                className="btn btn-outline-info btn-block" 
                 style={{textAlign:"left"}}
                 onClick={props.onClick_shelter_view}
                 >
@@ -15,10 +16,10 @@ const ShelterRow = (props) => {
                 </button>
             </div>
             {/* room manage button */}
-            <div className="col m-4">
+            <div className="col-md-2 p-0">
                 <button 
                 type="button"
-                className="btn btn-primary" 
+                className="btn btn-outline-info btn-block" 
                 style={{textAlign:"center"}}
                 onClick={props.onClick_room_manage}
                 >
@@ -26,10 +27,10 @@ const ShelterRow = (props) => {
                 </button>
             </div>
             {/* shelter update button */}
-            <div className="col m-4">
+            <div className="col-md-2 p-0">
                 <button 
                 type="button"
-                className="btn btn-primary" 
+                className="btn btn-outline-info btn-block" 
                 style={{textAlign:"center"}}
                 onClick={props.onClick_shelter_update}
                 >
@@ -37,10 +38,10 @@ const ShelterRow = (props) => {
                 </button>
             </div>
             {/* shelter delete button */}
-            <div className="col m-4">
+            <div className="col-md-2 p-0">
                 <button 
                 type="button"
-                className="btn btn-primary" 
+                className="btn btn-outline-info btn-block" 
                 style={{textAlign:"center"}}
                 onClick={props.onClick_shelter_delete}
                 >
