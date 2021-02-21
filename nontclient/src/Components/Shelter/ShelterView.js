f213d22bff356ae27d2cc2e9f5caf115a36bc145
pee34130 <pee34130@hotmail.com>
2021-02-21 20:15:16 +0700

    finish shelter and room view

--- nontclient/src/Components/Shelter/ShelterView.js
+++ nontclient/src/Components/Shelter/ShelterView.js
@@ -3,6 +3,8 @@ import ShelterService from "../../Services/ShelterService";
 import Contexts from "../../Utils/Context/Contexts";
 import { useParams } from "react-router-dom";
 import RoomService from "../../Services/RoomService";
+import UserType from "../../Constants/UserType";
+
 
 const UserContext = Contexts.UserContext;
 
@@ -49,7 +51,7 @@ const ShelterView = (props) => {
 
             <div class="card mt-3">
                 <div class="card-header text-white bg-primary ">
-                    <h1 className="my-1 text-left">{shelter?.name} </h1>
+                    <h1 className="my-1 text-left">{shelter.name} </h1>
 
                 </div>
                 <div class="card-body">
@@ -58,7 +60,7 @@ const ShelterView = (props) => {
                         <div class="media-body w-50">
                             <div class="mw-100">
                                 <h5 class="mb-1 mr-1">description </h5>
-                                <p class="mw-100">{shelter?.description}</p>
+                                <p class="mw-100">{shelter.description}</p>
                             <hr/>
                             </div>
                             <div>
@@ -77,19 +79,63 @@ const ShelterView = (props) => {
                             <hr class="mw-100"/>
                             </div>                            
                             <div>
-                                <h5 class="mb-1 mr-1">latitude </h5>
-                                <p>{shelter?.coordinate?.lat}</p>
-                            <hr class="mw-100"/>
+                                <h5 class="mb-1 mr-1">coordinate </h5>
+                                <p>{"latitude :"+shelter?.coordinate?.lat}</p>
+                                <p>{"longtitude:"+shelter?.coordinate?.lng}</p>
+                            <hr/>
                             </div> 
                             <div>
-                                <h5 class="mb-1 mr-1">longtitude </h5>
-                                <p>{shelter?.coordinate?.lng}</p>
+                                <h5 class="mb-1 mr-1">support type </h5>
+                                <div>
+                                {
+                                shelter?.supported_type?.map((type)=>{
+                                    return(
+                                        
+                                        <span class="badge badge-primary mr-1">{type}</span>
+                                        
+                                    );
+                                    
+                                }
+                                )
+                                }
+                                </div>
                             <hr class="mw-100"/>
-                            </div>                             
+                            </div>                                  
                         </div>
                     </div>
+                    <ul class="list-group">
+                        <li class="list-group-item active">room</li>
+                        {
+                            rooms.map((room)=>{
+                                return(
+                                    <li class="list-group-item">
+                                        
+                                        <dl class="row">
+                                            <dt class="col-md-2">name</dt>
+                                            <dd class="col-md-10">{room.name}</dd>
+                                            <dt class="col-md-2">nont type</dt>
+                                            <dd class="col-md-10"><span class="badge badge-primary mr-1">{room.nont_type}</span></dd>
+                                            <dt class="col-md-2">amount</dt>
+                                            <dd class="col-md-10">{room.amount}</dd>
+                                            <dt class="col-md-2">price</dt>
+                                            <dd class="col-md-10">{room.price}</dd>
+                                        </dl> 
+                                        {
+                                            contextValue.userType === UserType.NONT_OWNER&&(
+                                                <a class="btn btn-primary mt-0" href="#" role="button">reserve</a>
+
+                                            )
+                                        }       
+                                    </li>
+                                );
+                            })
+                        }
+
+                    </ul>                    
                 </div>
             </div>
+
+
         </div>
 
     );
