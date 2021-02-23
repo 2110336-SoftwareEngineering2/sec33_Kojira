f014866412e202aa68396c5e5664619bd3dde1ae
kittawat <kittawatsens@gmail.com>
2021-02-23 20:16:46 +0700

    add patch shelter in backend

--- nontserver/Controllers/ShelterController.js
+++ nontserver/Controllers/ShelterController.js
@@ -113,7 +113,24 @@ const controller = {
         catch(error){
             return res.status(500).send("Cannot create shelter");
         }
-    }
+    },
+    
+    // Patch /shelter
+    updateShelter: async (req, res) => {
+        try {
+          const data = req.body;
+
+          const Shelter= await Shelters.findByIdAndUpdate(
+            data._id,
+            { $set: data },
+            { new: true }
+          );
+          return res.send(_.pick(newShelter, ["_id","name","rate","phonenumber"]));
+        } catch (error) {
+          return res.status(500).send("Cannot access shelter.");
+        }
+      },
+
 }
 
 module.exports = controller;
