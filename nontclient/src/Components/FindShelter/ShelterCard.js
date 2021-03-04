84d9248ecf419d6e5e8456be793ccddae6a5bfd3
Megacodexz <megacodexz@gmail.com>
2021-03-04 19:22:46 +0700

    [FRONT] Add smooth transition in 'find shelter' cards

--- nontclient/src/Components/FindShelter/ShelterCard.js
+++ nontclient/src/Components/FindShelter/ShelterCard.js
@@ -1,15 +1,18 @@
 import React from "react";
+import styles from "./FindShelter.module.css";
 
 const ShelterCard = (props) => {
   const shelter = props.shelter;
   return (
-    <div className="card">
-      <img src="..." className="card-img-top" alt=""/>
-      <div className="card-body">
-        <h5 className="card-title">{shelter.name}</h5>
-        <p className="card-text">{shelter.description}</p>
+    <a className={styles.cardLink} href="#">
+      <div className={"card " + styles.shelterCard}>
+        <img src="..." className="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">{shelter.name}</h5>
+          <p className="card-text">{shelter.description}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
