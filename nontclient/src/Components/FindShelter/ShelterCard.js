e41f5ab6c0261d4cd44401fcd6b33654fd198085
Megacodexz <megacodexz@gmail.com>
2021-03-05 13:44:45 +0700

    [FRONT] Add name and rate to 'Find Shelter' cards

--- nontclient/src/Components/FindShelter/ShelterCard.js
+++ nontclient/src/Components/FindShelter/ShelterCard.js
@@ -4,12 +4,22 @@ import styles from "./FindShelter.module.css";
 const ShelterCard = (props) => {
   const shelter = props.shelter;
   return (
-    <a className={styles.cardLink} href="#">
-      <div className={"card " + styles.shelterCard}>
-        <img src="..." className="card-img-top" alt="" />
-        <div className="card-body">
-          <h5 className="card-title">{shelter.name}</h5>
-          <p className="card-text">{shelter.description}</p>
+    <a className={styles.cardLink} href={"/findShelter/" + shelter._id}>
+      <div
+        className={
+          "card d-flex justify-content-start align-items-center " +
+          styles.shelterCard
+        }
+      >
+        <div className={"d-flex " + styles.image}></div>
+        <div className={"d-flex " + styles.shelterName}>{shelter.name}</div>
+        <div className="d-flex">
+          <span className={"mr-2  " + styles.fade}>Rate</span>
+          {[1, 2, 3, 4, 5].map((rating) => {
+            if (Math.round(shelter.rate) >= rating) return <i className={"fas fa-star " + styles.star}></i>;
+            else return <i className={"far fa-star " + styles.star + " " + styles.fade}></i>;
+          })}
+          <span className={"ml-2  " + styles.fade}>{shelter.rate}</span>
         </div>
       </div>
     </a>
