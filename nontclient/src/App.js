db2dcdea507b40fa368502a70dffd62c1355853c
kombangkoe <kbdirifilation@gmail.com>
2021-03-06 16:16:27 +0700

    Revert "tried react-router-guards library, still not working as expected, can't go into UserSetting without getting the idiosyncratic warning"
    This reverts commit dad302cee95e024dcdf1ccf52f79a1500a49e8ee.

--- nontclient/src/App.js
+++ nontclient/src/App.js
@@ -6,7 +6,7 @@ import LoginService from "./Services/LoginService";
 
 const UserContext = Contexts.UserContext;
 
-const App = () => {
+const App = (props) => {
   const [userInfo, setUserInfo] = useState({
     userType: null,
     email: null,
--- nontclient/src/Router/mainRouter.js
+++ nontclient/src/Router/mainRouter.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { GuardProvider, GuardedRoute } from "react-router-guards";
 import Dashboard from "../Components/Dashboard/Dashboard";
 import Login from "../Components/LoginLogout/Login";
 import Registration from "../Components/Registration/Registration";
@@ -11,105 +10,17 @@ import RoomManage from "../Components/Room/RoomManage";
 import RoomUpdate from "../Components/Room/RoomUpdate";
 import ShelterView from "../Components/Shelter/ShelterView";
 
-import LoginService from "../Services/LoginService";
+import Contexts from "../Utils/Context/Contexts";
 
-import UserType from "../Constants/UserType";
+const UserContext = Contexts.UserContext;
 
-const requireNontOwner = (to, from, next) => {
-  if (to.meta.auth) {
-    LoginService.checkLoginStatus().then((respond) => {
-      if (
-        respond.status === 200 &&
-        respond.data.userType === UserType.NONT_OWNER
-      ) {
-        next();
-      }
-      console.log(respond.data);
-      next.redirect("/home");
-    });
-  } else {
-    next();
-  }
-};
+function Router(props) {
+  const value = useContext(UserContext);
 
-const requireNontSitter = (to, from, next) => {
-  if (to.meta.auth) {
-    LoginService.checkLoginStatus().then((respond) => {
-      if (
-        respond.status === 200 &&
-        respond.data.userType === UserType.NONT_SITTER
-      ) {
-        next();
-      }
-      next.redirect("/home");
-    });
-  } else {
-    next();
-  }
-};
-
-function Router() {
   return (
     <BrowserRouter>
-      {/* <React.Fragment>
-        <GuardProvider guards={[requireNontOwner]}>
-          <Switch>
-            <GuardedRoute
-              path="/nont"
-              component={() => <h1>Nonts</h1>}
-              meta={{ auth: true }}
-            />
-            <GuardedRoute
-              path="/reserve"
-              component={() => <h1>Reserve</h1>}
-              meta={{ auth: true }}
-            />
-            <GuardedRoute
-              path="/setting"
-              component={UserSetting}
-              meta={{ auth: true }}
-            />
-          </Switch>
-        </GuardProvider>
-        <GuardProvider guards={[requireNontSitter]}>
-          <Switch>
-            <Route
-              path="/room/register/:shelterID"
-              component={RoomRegistration}
-              meta={{ auth: true }}
-            />
-            <GuardedRoute path="/room/update/:roomID" component={RoomUpdate} />
-            <GuardedRoute
-              path="/room/manage/:shelterID"
-              component={RoomManage}
-              meta={{ auth: true }}
-            />
-            <GuardedRoute
-              path="/shelter"
-              component={ShelterManage}
-              meta={{ auth: true }}
-            />
-            <GuardedRoute
-              path="/shelterView/:shelterID"
-              component={ShelterView}
-              meta={{ auth: true }}
-            />
-            <GuardedRoute
-              path="/setting"
-              component={UserSetting}
-              meta={{ auth: true }}
-            />
-          </Switch>
-        </GuardProvider>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Registration} />
-          <Route path="/home" component={Dashboard} />
-        </Switch>
-      </React.Fragment> */}
-
       <Switch>
-        <Route path="/nont" component={() => <h1>Nonts</h1>} />
+        <Route path="/nont" component={<h1>Nonts</h1>} />
         <Route path="/room/register/:shelterID" component={RoomRegistration} />
         <Route path="/room/update/:roomID" component={RoomUpdate} />
         <Route path="/room/manage/:shelterID" component={RoomManage} />
