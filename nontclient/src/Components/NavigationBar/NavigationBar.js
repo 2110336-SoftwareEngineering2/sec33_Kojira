a40218ae0689583a0db738b0ffee180391141440
jelly <32786620+puttimeth@users.noreply.github.com>
2021-03-12 19:12:19 +0700

    Redirect logo button
    - now click logo button will redirect to '/home' if you are not login and redirect to '/dashboard' if you are login

--- nontclient/src/Components/NavigationBar/NavigationBar.js
+++ nontclient/src/Components/NavigationBar/NavigationBar.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import NavigationLinks from "./NavigationLinks";
 import styles from "./NavigationBar.module.css";
+import Contexts from "../../Utils/Context/Contexts";
+import UserType from "../../Constants/UserType";
+
+const UserContext = Contexts.UserContext;
 
 const NavigationBar = () => {
+  const value = useContext(UserContext);
   return (
     <nav className="navbar navbar-expand-sm navbar-dark" id={styles.topNavBar}>
-      <a className="navbar-brand" href="/home">
-        <img src="/nont-logo.svg" id={styles.logo} alt="Nont logo"></img>
-      </a>
+      {
+        (value.userType === UserType.NONT_OWNER || value.userType === UserType.NONT_SITTER) &&
+        <a className="navbar-brand" href="/dashboard">
+          <img src="/nont-logo.svg" id={styles.logo} alt="Nont logo"></img>
+        </a>
+      }
+      {
+        (value.userType !== UserType.NONT_OWNER && value.userType !== UserType.NONT_SITTER) &&
+        <a className="navbar-brand" href="/home">
+          <img src="/nont-logo.svg" id={styles.logo} alt="Nont logo"></img>
+        </a>
+      }
       <button
         className="navbar-toggler"
         type="button"
