1fbe88efcfedd68ef9b94970a9d65d64d9308d5b
Megacodexz <megacodexz@gmail.com>
2021-03-12 17:30:36 +0700

    [FRONT] Add list template in Nont owner's dashboard

--- nontclient/src/Components/Dashboard/NontOwnerDashboard.js
+++ nontclient/src/Components/Dashboard/NontOwnerDashboard.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const NontOwnerDashboard = () => {
-  return ( 
-    <h1>Nont Owner Dashboard</h1>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center m-3">
+          <h1>Nont Owner Dashboard</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">
+              Link list
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
- 
+};
+
 export default NontOwnerDashboard;
