ce454802c21371f50374be0b50c18218c0566174
pee34130 <pee34130@hotmail.com>
2021-03-13 22:00:23 +0700

    fix responsive but fail to fix image

--- nontclient/src/Components/Shelter/ShelterView.js
+++ nontclient/src/Components/Shelter/ShelterView.js
@@ -4,10 +4,31 @@ import Contexts from "../../Utils/Context/Contexts";
 import { useParams } from "react-router-dom";
 import RoomService from "../../Services/RoomService";
 import UserType from "../../Constants/UserType";
-
-
+        
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    let bytes = new Uint8Array(buffer?.data);
+    let len = bytes.byteLength;
+    /*
+    console.log("----Infunction----");
+    console.log(buffer?.data);
+    console.log("typeofbuffer")
+    console.log(typeof(bytes))
+    console.log("lengthofbuffer");
+    console.log(len);
+    console.log("----Endoffunction----");
+    */
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }    
+    return window.btoa(binary);
+}
+// function ToBase64(bufferdata) {
+//     console.log(typeof(bufferdata));
+//     var thumb = new Buffer.from(bufferdata).toString('base64');
+//     return thumb;
+// }
 const UserContext = Contexts.UserContext;
-
 const ShelterView = (props) => {
     const contextValue = useContext(UserContext);
     const [shelter, setShelter] = useState([]);
@@ -22,6 +43,15 @@ const ShelterView = (props) => {
                         setShelter(response.data);
                     }
                 } 
+                
+                console.log("---useEffect3----");
+                console.log(shelter?.picture?.[0]?.img);
+                console.log(shelter?.picture?.[0]?.img?.data);
+                console.log(typeof (shelter?.picture?.[0]?.img.data));
+                console.log(arrayBufferToBase64(shelter?.picture?.[0]?.img));
+                console.log(Buffer.from(shelter?.picture?.[0]?.img?.data).toString('base64'))
+                console.log("---EndOFuseEffect3----");
+                
             }
             catch (error) {
                 console.error(error.message);
@@ -52,58 +82,66 @@ const ShelterView = (props) => {
             <div className="card mt-3">
                 <div className="card-header text-white bg-primary ">
                     <h1 className="my-1 text-left">{shelter.name} </h1>
-
                 </div>
                 <div className="card-body">
-                    <div className="media">
-                    <img className="align-self-start mr-3 w-50" src="" alt="shelter image"/>
-                        <div className="media-body w-50">
-                            <div className="mw-100">
-                                <h5 className="mb-1 mr-1">description </h5>
-                                <p className="mw-100">{shelter.description}</p>
-                            <hr/>
-                            </div>
-                            <div>
-                                <h5 className="mb-1 mr-1">phone number </h5>
-                                <p>{shelter.phoneNumber}</p>
-                            <hr className="mw-100"/>
-                            </div> 
-                            <div>
-                                <h5 className="mb-1 mr-1">address </h5>
-                                <p>{shelter.address}</p>
-                            <hr className="mw-100"/>
-                            </div>
-                            <div>
-                                <h5 className="mb-1 mr-1">rate </h5>
-                                <p>{shelter.rate}</p>
-                                <i className="fas fa-star-half-alt-warning"></i>
-
-
-                            <hr className="mw-100"/>
-                            </div>                            
-                            <div>
-                                <h5 className="mb-1 mr-1">coordinate </h5>
-                                
-                                <p>{"latitude :"+shelter?.coordinate?.lat}</p>
-                                <p>{"longtitude:"+shelter?.coordinate?.lng}</p>
-                                 
-                            <hr/>
-                            </div> 
-                            <div>
-                                <h5 className="mb-1 mr-1">support type </h5>
-                                <div>
-                                {
-                                shelter?.supported_type?.map((type)=>{
-                                    return(                                        
-                                        <span className="badge badge-primary mr-1" key={type}>{type}</span>                                        
-                                    );
-                                    
-                                }
-                                )
-                                }
+                    <div className="row">
+                        <div className="col-md-6">
+                        <img className="align-self-start mr-3 w-50" src={`data:${shelter?.picture?.[0]?.contentType};base64,${arrayBufferToBase64(shelter?.picture?.[0]?.img)}`} alt="shelter image"/>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="media-body w-50">
+                                <div className="mw-100">
+                                    <h5 className="mb-1 mr-1">description </h5>
+                                    <p className="mw-100">{shelter.description}</p>
+                                <hr/>
                                 </div>
-                            <hr className="mw-100"/>
-                            </div>                                  
+                                <div>
+                                    <h5 className="mb-1 mr-1">phone number </h5>
+                                    <p>{shelter.phoneNumber}</p>
+                                <hr className="mw-100"/>
+                                </div> 
+                                <div>
+                                    <h5 className="mb-1 mr-1">address </h5>
+                                    <p>{shelter.address}</p>
+                                <hr className="mw-100"/>
+                                </div>
+                                <div>
+                                    <h5 className="mb-1 mr-1">rate </h5>
+                                    <p>{shelter.rate}</p>
+                                    <i className="fas fa-star-half-alt-warning"></i>
+
+
+                                <hr className="mw-100"/>
+                                </div>                            
+                                <div>
+                                    <h5 className="mb-1 mr-1">coordinate </h5>
+                                    
+                                    <p>{"latitude :"+shelter?.coordinate?.lat}</p>
+                                    <p>{"longtitude:"+shelter?.coordinate?.lng}</p>
+                                    {/* <p>{`data:${shelter?.picture[0]?.contentType};base64,${shelter?.picture[0]?.name}`}</p> */}
+                                    <p>{`data:${shelter?.picture?.[0]?.contentType};base64,`}</p>
+                                    <p>{//Buffer.from(shelter?.picture?.[0]?.img?.data).toString('base64')
+                                    }</p>
+                                    
+                                    
+                                <hr/>
+                                </div> 
+                                <div>
+                                    <h5 className="mb-1 mr-1">support type </h5>
+                                    <div>
+                                    {
+                                    shelter?.supported_type?.map((type)=>{
+                                        return(                                        
+                                            <span className="badge badge-primary mr-1" key={type}>{type}</span>                                        
+                                        );
+                                        
+                                    }
+                                    )
+                                    }
+                                    </div>
+                                <hr className="mw-100"/>
+                                </div>                                  
+                            </div>
                         </div>
                     </div>
                     <ul className="list-group">
