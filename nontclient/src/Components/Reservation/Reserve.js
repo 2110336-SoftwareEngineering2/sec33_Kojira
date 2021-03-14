9d406ca5587f2639531e8ac0549f28a0a26af2c7
jelly <32786620+puttimeth@users.noreply.github.com>
2021-03-14 23:21:47 +0700

    add Reserve Form

--- nontclient/src/Components/Reservation/Reserve.js
+++ nontclient/src/Components/Reservation/Reserve.js
@@ -5,7 +5,7 @@ import ReservationService from "../../Services/ReserveService";
 import RoomService from "../../Services/RoomService";
 import styles from "./Reserve.module.css";
 import moment from "moment";
-import { DatePicker } from "antd";
+import { DatePicker, Select, Statistic } from "antd";
 
 const UserContext = Contexts.UserContext;
 
@@ -13,6 +13,7 @@ const Reserve = (props) => {
     const [rooms, setRooms] = useState({name:"default room name"});
     const { roomID } = useParams();
     const { RangePicker } = DatePicker;
+    const {Option} = Select;
 
     useEffect(() => {
         fetchRooms();
@@ -32,16 +33,71 @@ const Reserve = (props) => {
         }
     }
 
+    const disabledDate = (current) => {
+        return current < moment();
+    }
+
+    const submitReserve = async () => {
+
+    }
+
     return(
         <div 
         className="container mt-3" 
         style={{backgroundColor:"#7ed6df", borderRadius:20}}>
             <h1 style={{textAlign:"center", color:"#30336b"}}>{rooms.name}</h1>
             <div className="row">
-            <div className="col col-md-12" style={{backgroundColor:"#dfe6e9", borderRadius:20}}>
-                b
-                <RangePicker showTime disabledDate={}/>
-            </div>
+                <div className="col col-md-12" style={{backgroundColor:"#f5f6fa", borderRadius:20, textAlign:"center"}}>
+                    {/* Nont Select */}
+                    <div className="row m-2">                        
+                        <div className="col m-2 col-sm">
+                            <label style={{fontSize:"15px", padding:"10px"}}>
+                                Nonts
+                            </label>   
+                            <Select
+                            mode="multiple"
+                            placeholder="Press select at least 1 Nont"
+                            size="large"
+                            style={{maxWidth:"300px", width:"50%"}}
+                            >
+                                <Option key="test1">test1</Option>
+                                <Option key="test2">test2</Option>
+                                <Option key="test3">test3</Option>
+                                <Option key="test4">test4</Option>
+                                <Option key="test5">test5</Option>
+                                <Option key="test6">test6</Option>
+                                <Option key="test7">test7</Option>
+                                <Option key="test8">test8</Option>
+                            </Select>                         
+                        </div>                    
+                    </div>
+                    {/* Reservation Time Pick */}
+                    <div className="row">
+                        <div className="col md-2 col-sm">
+                            <label style={{padding:"10px"}}>
+                                Reservation Time
+                            </label>
+                            <RangePicker size="large" disabledDate={disabledDate} format="DD-MM-YYYY" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col m-2 col-sm">
+                            <Statistic title="Price" value={0} suffix="baht" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col m-2 col-sm">
+                            <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={submitReserve}
+                            >
+                            Reserve Room
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
