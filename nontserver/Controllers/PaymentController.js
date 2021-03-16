d7e05b0d6c35eee5423d2c0d9d012c56c736530b
kombangkoe <kbdirifilation@gmail.com>
2021-03-16 19:23:07 +0700

    resolve conflicts

---
+++ nontserver/Controllers/PaymentController.js
@@ -0,0 +1,22 @@
+const Reservation = require("../Models/Reservation");
+const RandomCodes = require("random-codes");
+const rc = new RandomCodes();
+
+const controller = {
+  payment: async (req, res) => {
+    var validated_code = rc.validate(req.query.code);
+    if (validated_code !== req.query.code) {
+      res.send("code not match");
+    } else {
+      //console.log("QR scanned");
+      const reservation = await Reservation.findById(req.params.id);
+      await Reservation.updateOne(
+        { _id: req.query.reserveId },
+        { status: "paid" }
+      );
+      res.send("payment finished");
+    }
+  },
+};
+
+module.exports = controller;
