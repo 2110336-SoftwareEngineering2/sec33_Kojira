bbd7186236e8c0b446a0742a9306acf382beb220
mmkanta <47110972+mmkanta@users.noreply.github.com>
2021-03-17 10:03:26 +0700

    [front] add history

--- nontclient/src/Components/Dashboard/NontSitterDashboard/ReservationList.js
+++ nontclient/src/Components/Dashboard/NontSitterDashboard/ReservationList.js
@@ -15,6 +15,7 @@ const ReservationList = () => {
   }, [contextValue]);
 
   const nontSitterID = contextValue._id;
+  const [expand, setExpand] = useState(false);
 
   useEffect( () => {  
     fetchReservations();   
@@ -40,8 +41,7 @@ const ReservationList = () => {
         <div className="list-group">
           <div className="row">
             {reservations.map((reservation) => {
-              if(reservation.status==="cancelled") return
-              else{
+              if(reservation.status!=="cancelled"){
                 return(
                   <div className="col-lg-6">
                     <a href={"/reserveInfo/" + reservation._id} className="m-2">
@@ -79,6 +79,75 @@ const ReservationList = () => {
                 )
               }
             })}
+          </div>
+        </div>
+
+        <div className="mt-5" style={{ marginBottom: "10rem" }}>
+          <div
+            className={styles.toggleable}
+            onClick={() => {
+              setExpand(!expand);
+            }}
+          >
+            <span className="ml-3" id={styles.historyTitle}>
+              <i
+                className={
+                  "fa fa-angle-right mr-3 " +
+                  (expand ? styles.downDropdown : styles.rightDropdown)
+                }
+              ></i>
+              History
+            </span>
+            <hr className={styles.horizontalLine} />
+          </div>
+          <div
+            className={
+              styles.collapsible + " " + (!expand ? styles.collapse : styles.expand)
+            }
+          >
+            <div className="list-group">
+            <div className="row">
+              {reservations.map((reservation) => {
+                if(reservation.status==="cancelled"){
+                  return(
+                    <div className="col-lg-6">
+                      <a href={"/reserveInfo/" + reservation._id} className="m-2">
+                        <div className={"card mt-3 " + styles.reservationCard}>
+                          <div className="card-header text-white bg-primary" style={{fontSize: "20px", fontWeight: '600'}}>
+                            {_.truncate(reservation.shelter_id.name?reservation.shelter_id.name:"Shelter", { length: 20 })}
+                          </div>
+                          <div className="card-body">
+                            <h5 className="card-title">
+                              {_.truncate(reservation.nontowner_id.name?reservation.nontowner_id.name:"Owner", { length: 20 })}
+                            </h5>
+                            <div className="row align-items-center">
+                              <div className="col-5 text-center" style={{fontSize: "18px", fontWeight: '600', color: "black", opacity: "0.8"}}>
+                                {reservation.status?reservation.status:"?"}
+                              </div>
+                              <div className="col-7" style={{fontSize: "15px", color: "black", opacity: "0.7"}}>
+                                <div className="row">
+                                  <strong>Room</strong>{": " + _.truncate(reservation.room_id.name?reservation.room_id.name:"Room", { length: 20 })}
+                                </div>
+                                <div className="row">
+                                  <strong>Amount</strong>{": " + reservation.nont_id.length + " " + _.truncate(reservation.nont_id[0].type) + (reservation.nont_id.length==1?"":"s")}
+                                </div>
+                                <div className="row">
+                                  <strong>From</strong>{": " + _.truncate(reservation.start_datetime?reservation.start_datetime.slice(0,15):"XX/XX/XX")}
+                                </div>
+                                <div className="row">
+                                  <strong>To</strong>{": " + _.truncate(reservation.start_datetime?reservation.end_datetime.slice(0,15):"XX/XX/XX")}
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  )
+                }
+              })}
+            </div>
+          </div>
           </div>
         </div>
     </React.Fragment>
