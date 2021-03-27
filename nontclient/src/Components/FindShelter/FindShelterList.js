b3f5a255d1f4dc97d553491693236bdc6e73191f
Megacodexz <megacodexz@gmail.com>
2021-03-27 12:27:55 +0700

    [FRONT] Add dropdown for 'Sort by'

--- nontclient/src/Components/FindShelter/FindShelterList.js
+++ nontclient/src/Components/FindShelter/FindShelterList.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import styles from "./FindShelter.module.css";
+import { Menu, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import ShelterCard from "./ShelterCard";
 import ShelterFilter from "./ShelterFilter/ShelterFilter";
 
 const FindShelterList = (props) => {
   const [filteredShelters, setFilteredShelters] = useState([]);
   const [displayedShelters, setDisplayedShelters] = useState([]);
+  const [sortedBy, setSortedBy] = useState("Name");
 
   // Set default filtered shelters
   useEffect(() => {
@@ -16,6 +19,14 @@ const FindShelterList = (props) => {
   useEffect(() => {
     setDisplayedShelters(filteredShelters);
   }, [filteredShelters]);
+
+  const sortedByMenu = (
+    <Menu>
+      <Menu.Item>
+        <button className={styles.textButton}>Name</button>
+      </Menu.Item>
+    </Menu>
+  );
 
   return (
     <React.Fragment>
@@ -28,9 +39,25 @@ const FindShelterList = (props) => {
         </div>
       </div>
       <hr />
-      <span className={styles.fade}>
-        Showing {displayedShelters.length} results
-      </span>
+      <div className="d-flex justify-content-between">
+        <div className="d-flex">
+          <span className={styles.fade}>
+            Showing {displayedShelters.length} results
+          </span>
+        </div>
+        <div className="d-flex">
+          <span className={"mr-1 " + styles.fade}>Sorted by</span>
+          <Dropdown overlay={sortedByMenu}>
+            <button
+              className={"ant-dropdown-link " + styles.textButton}
+              onClick={(e) => e.preventDefault()}
+            >
+              <span className="mr-1">{sortedBy}</span>
+              <DownOutlined />
+            </button>
+          </Dropdown>
+        </div>
+      </div>
       <div className="row">
         {displayedShelters.map((shelter) => (
           <div
