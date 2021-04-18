3e1c7c0e29fd4f52709282070cf89c347acff78b
Sakon Thephamongkhol <kbdirifilation@gmail.com>
2021-04-18 19:55:37 +0700

    fix swagger and add some fields

--- nontserver/Controllers/PaymentController.js
+++ nontserver/Controllers/PaymentController.js
@@ -6,14 +6,20 @@ const controller = {
   payment: async (req, res) => {
     var validated_code = rc.validate(req.query.code);
     if (validated_code !== req.query.code) {
+      res.status(401);
       res.send("code not match");
     } else {
       //console.log("QR scanned");
-      await Reservation.updateOne(
-        { _id: req.query.reserveId },
-        { status: "paid" }
-      );
-      res.send("payment finished");
+      try {
+        await Reservation.updateOne(
+          { _id: req.query.reserveId },
+          { status: "paid" }
+        );
+        res.send("payment finished");
+      } catch (error) {
+        res.status(500);
+        res.send("unexpected error");
+      }
     }
   },
   getCode: (req, res) => {
