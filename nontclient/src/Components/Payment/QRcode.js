60d9315d73776025f31a6358fa21c649a2209c62
Sakon Thephamongkhol <kbdirifilation@gmail.com>
2021-04-26 20:53:34 +0700

    the payment authorization now seems to work

--- nontclient/src/Components/Payment/QRcode.js
+++ nontclient/src/Components/Payment/QRcode.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axios } from "../../Utils/ServiceUtils/axios";
 import serverURL from "../../Config/serverURL";
 
 var QRCode = require("qrcode.react");
