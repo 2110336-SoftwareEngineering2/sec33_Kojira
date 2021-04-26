9bc1ee64accf802a30271ef8c4c10baa74b05fcf
jelly <32786620+puttimeth@users.noreply.github.com>
2021-04-26 09:33:37 +0700

    [back] remove comment in NontOwnerUpdate test

--- nontserver/test/LoginAndSignup/NontOwnerUpdate.test.js
+++ nontserver/test/LoginAndSignup/NontOwnerUpdate.test.js
@@ -106,7 +106,6 @@ describe("It should not update the user that is not existed", (done) => {
         email: "nontOwnerTestUpdate2@kojira.com",
       })
       .end((err, res) => {
-        console.log(res.body);
         expect(res).to.have.status(404);
         done();
       });
