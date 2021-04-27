8c321f5d2d861ba3ca9ea26325d6b1e5cbb90da8
Megacodexz <megacodexz@gmail.com>
2021-04-27 14:08:28 +0700

    [BACK] Update FindShelter.test.js

--- nontserver/test/Shelter/FindShelter.test.js
+++ nontserver/test/Shelter/FindShelter.test.js
@@ -197,4 +197,326 @@ describe("Find Shelter", () => {
       });
   });
 
+  it("TC1-14 It should send an error when startDate is not in the format", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "20XX-01-01",
+        endDate: "2022-02-02"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-15 It should send an error when month = 0 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-00-01",
+        endDate: "2022-02-02"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-16 It should send an error when month > 12 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-13-01",
+        endDate: "2022-02-02"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-17 It should send an error when day = 0 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-00",
+        endDate: "2022-02-02"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-18 It should send an error when day > 31 if month = 1, 3, 5, 7, 8, 10, or 12 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-32",
+        endDate: "2022-02-02"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-19 It should send an error when day > 30 if month = 4, 6, 9, or 11 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-04-31",
+        endDate: "2022-06-01"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-20 It should send an error when day > 28 if month = 2 and year % 4 != 0 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-02-29",
+        endDate: "2022-04-01"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-21 It should send an error when day > 29 if month = 2 and year % 4 == 0 and year % 100 != 0 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2024-02-30",
+        endDate: "2024-04-01"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-22 It should send an error when day > 28 if month = 2 and year % 4 == 0 and year % 100 == 0 and year % 400 != 0 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2100-02-29",
+        endDate: "2100-04-01"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-23 It should send an error when day > 28 if month = 2 and year % 4 == 0 and year % 100 == 0 and year % 400 == 0 (startDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2400-02-30",
+        endDate: "2400-04-01"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid startDate');
+        done();
+      });
+  });
+
+  it("TC1-24 It should send an error when endDate is not in the format", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2022-02-XX"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-25 It should send an error when month = 0 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2023-00-01"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-26 It should send an error when month > 12 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2023-13-01"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-27 It should send an error when day = 0 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2023-01-00"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-28 It should send an error when day > 31 if month = 1, 3, 5, 7, 8, 10, or 12 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2023-01-32"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-29 It should send an error when day > 30 if month = 4, 6, 9, or 11 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2023-04-31"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-30 It should send an error when day > 28 if month = 2 and year % 4 != 0 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2023-02-29"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-31 It should send an error when day > 29 if month = 2 and year % 4 == 0 and year % 100 != 0 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2024-02-30"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-32 It should send an error when day > 28 if month = 2 and year % 4 == 0 and year % 100 == 0 and year % 400 != 0 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2100-02-29"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+  it("TC1-33 It should send an error when day > 28 if month = 2 and year % 4 == 0 and year % 100 == 0 and year % 400 == 0 (endDate)", (done) => {
+    chai
+      .request(app)
+      .get("/shelter/findShelters")
+      .query({
+        startDate: "2022-01-01",
+        endDate: "2400-02-30"
+      })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.equal('Invalid endDate');
+        done();
+      });
+  });
+
+
+
 });
